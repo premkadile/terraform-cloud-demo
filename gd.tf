77ece9c292c33a68c7055b33f18a1c016e4f1e74
premkadile <55544831+premkadile@users.noreply.github.com>
2021-01-14 18:43:49 +0530

    Update gd.tf

--- gd.tf
+++ gd.tf
@@ -1,3 +1,20 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.23.0"
+    }
+  }
+}
+
+provider "aws" {
+region  = var.region
+}
+
 #test
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
