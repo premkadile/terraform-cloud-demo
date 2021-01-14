9468daed6eb9c088839105fc716038d476016f0f
premkadile <55544831+premkadile@users.noreply.github.com>
2021-01-14 18:31:20 +0530

    Update gd.tf

--- gd.tf
+++ gd.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.23.0"
-    }
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
