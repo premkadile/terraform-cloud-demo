2cd61e20f44f2d844ad24619f76ca293c6f4433a
premkadile <55544831+premkadile@users.noreply.github.com>
2021-01-14 18:47:01 +0530

    Update gd.tf

--- gd.tf
+++ gd.tf
@@ -2,15 +2,6 @@ variable "region" {
   default = "us-west-2"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.23.0"
-    }
-  }
-}
-
 provider "aws" {
 region  = var.region
 }
